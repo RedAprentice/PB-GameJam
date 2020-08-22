6b96c8d0c4d9701f73f96d3b89b3fb417c067f28
Steven Huynh <steven.h.mainacc@gmail.com>
2020-08-22 02:03:59 -0500

    Added Skeleton Script for Kill Plane
    This will stand in for "Pits"

---
+++ PB-GameJam/Assets/Scripts/KillPlane.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlane : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            // trigger the death sequence
+        }
+    }
+}
