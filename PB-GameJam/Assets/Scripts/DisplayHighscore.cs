c171d4652c2831b507193e895043f03f1ec13151
Steven Huynh <steven.h.mainacc@gmail.com>
2020-08-24 01:55:46 -0500

    Finished Highscore
    Script that is attached to the text that will be the highscore

---
+++ PB-GameJam/Assets/Scripts/DisplayHighscore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayHighscore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TimeSpan timeSpan = TimeSpan.FromSeconds(Score.Instance.highScore());
+        TimeSpan timeSpan = TimeSpan.FromSeconds(1.1f);
+        gameObject.GetComponent<Text>().text = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds / 10);
+    }
+}
