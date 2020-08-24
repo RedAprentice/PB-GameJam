e13e8214c5b29f88f868697a7daeeba3c2e24672
Steven Huynh <steven.h.mainacc@gmail.com>
2020-08-24 03:00:22 -0500

    Added Basic Button Functions

--- PB-GameJam/Assets/Scripts/ButtonFunctions.cs
+++ PB-GameJam/Assets/Scripts/ButtonFunctions.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class ButtonFunctions : MonoBehaviour
+public class ButtonFunctions
 {
-    // Start is called before the first frame update
-    void Start()
+    public void playButton()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void mainMenuButton()
     {
-        
+        SceneManager.LoadScene(0);
+    }
+
+    public void quitGameButton()
+    {
+        Application.Quit();
+    }
+
+    public void replayButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
