bb9f6cbae7cc8b92f7c0a53ef02321a2eaed2983
Steven Huynh <steven.h.mainacc@gmail.com>
2020-08-24 21:38:46 -0500

    Added Music and Attributions

--- PB-GameJam/Assets/Scripts/Music.cs
+++ PB-GameJam/Assets/Scripts/Music.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Music : MonoBehaviour
 {
+
+    public static Music Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject); // ensures that only one Instance can exist
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
